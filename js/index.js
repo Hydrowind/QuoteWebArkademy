3aa877a7a146df50647563b31ab00faf0ad74002
Herdhiantoko Fathani <herdhiantokofathani@gmail.com>
2020-08-27 06:58:55 +0700

    :rocket: [Add] Responsive & Error Message

--- js/index.js
+++ js/index.js
@@ -1,22 +1,27 @@
 const API_URL = "https://programming-quotes-api.herokuapp.com/quotes/lang/en";
+const COLORS = ["red", "black", "brown", "green", "blue", "orange"];
 
 var data;
 var current;
 
+var quot = document.querySelector("#quot");
 var quote = document.querySelector("#quote");
 var author = document.querySelector("#author");
 
+changeBackground();
 fetch(API_URL)
   .then((response) => response.json())
   .then((result) => {
-    data = result;
+    data = result.filter((x) => x.en.length <= 150);
     current = Math.floor(Math.random() * data.length);
 
     quote.innerHTML = data[current].en;
     author.innerHTML = data[current].author;
+  })
+  .catch((err) => {
+    showError();
+    console.log(err);
   });
-
-changeBackground();
 
 function showLoading() {
   let card = document.querySelector(".card");
@@ -30,34 +35,65 @@ function showLoading() {
 function hideLoading() {
   let card = document.querySelector(".card");
   for (child of card.children) {
-    child.style.display = "block";
+    child.style.display = "";
   }
   let spinner = document.querySelector("#spinner");
   spinner.style.display = "none";
 }
 
+function showError() {
+  let card = document.querySelector(".card");
+  for (child of card.children) {
+    child.style.display = "none";
+  }
+  let error = document.querySelector("#error");
+  error.style.display = "block";
+  document.querySelector("#btn-prev").style.display = "none";
+  document.querySelector("#btn-next").style.display = "none";
+}
+
+function hideError() {
+  let card = document.querySelector(".card");
+  for (child of card.children) {
+    child.style.display = "";
+  }
+  let error = document.querySelector("#error");
+  error.style.display = "none";
+}
+
 function changeBackground() {
   showLoading();
-  fetch("https://picsum.photos/1024/576.webp").then((response) => {
-    document.body.style.backgroundImage = `url('${response.url}')`;
-    hideLoading();
-  });
+  fetch("https://picsum.photos/1024/576.webp")
+    .then((response) => {
+      document.body.style.backgroundImage = `url('${response.url}')`;
+      hideLoading();
+    })
+    .catch((err) => {
+      document.body.style.backgroundImage = `url('/assets/background.jpg')`;
+      hideLoading();
+    });
 }
 
 function nextQuote() {
   changeBackground();
-  current++;
-  current %= data.length;
-  quote.innerHTML = data[current].en;
-  author.innerHTML = data[current].author;
+  if (data) {
+    quot.children[0].style.fill = COLORS[current % COLORS.length];
+    current++;
+    current %= data.length;
+    quote.innerHTML = data[current].en;
+    author.innerHTML = "- " + data[current].author;
+  }
 }
 
 function prevQuote() {
   changeBackground();
-  current--;
-  if (current < 0) current = data.length;
-  quote.innerHTML = data[current].en;
-  author.innerHTML = data[current].author;
+  if (data) {
+    quot.children[0].style.fill = COLORS[current % COLORS.length];
+    current--;
+    if (current < 0) current = data.length;
+    quote.innerHTML = data[current].en;
+    author.innerHTML = data[current].author;
+  }
 }
 
 document.querySelector("#btn-next").addEventListener("click", nextQuote);
